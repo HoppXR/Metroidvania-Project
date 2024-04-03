cb79c03b11372eba8fdc72de80032b49eec3f581
Dean <40185661+DeanSatine@users.noreply.github.com>
2024-04-03 16:46:55 -0400

    Boss Mid Fight dialogues except aves

--- Assets/Scripts/Richard/BossHealthIoko.cs
+++ Assets/Scripts/Richard/BossHealthIoko.cs
@@ -25,6 +25,8 @@ public class BossHealthIoko : MonoBehaviour
     [SerializeField] public GameObject enablePortal;
     [SerializeField] public GameObject deathDialogue;
     [SerializeField] public GameObject bossBarrier;
+    [SerializeField] private GameObject specialEffectObject;
+    private bool specialEffectActivated = false;
 
     void Start()
     {
@@ -86,6 +88,14 @@ public class BossHealthIoko : MonoBehaviour
         healthSlider.value = _currentHealth;
 
         Instantiate(blood, transform.position, Quaternion.identity);
+
+
+        if (_currentHealth <= maxHealth * 0.5f && !specialEffectActivated)
+        {
+            specialEffectObject.SetActive(true);
+            specialEffectActivated = true;
+            Destroy(specialEffectObject, 1f);
+        }
 
         if (_currentHealth <= 0)
         {
